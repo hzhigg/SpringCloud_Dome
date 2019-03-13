52bd7d4325dcbcf77e681b74822a5b7aa9bdec5e
huangzhi <huangz@g2l-service.com>
2019-03-13 11:29:47 +0800

    解决配置中心刷新不生效问题

--- eureka-client/src/main/java/com/demo/user/controller/DcController.java
+++ eureka-client/src/main/java/com/demo/user/controller/DcController.java
@@ -3,11 +3,13 @@ package com.demo.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 
+@RefreshScope//刷新配置中心生效注解
 @RestController
 public class DcController {
 
